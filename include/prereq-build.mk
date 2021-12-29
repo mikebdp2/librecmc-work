dc6d69b65d6003957ea74df9d3456322a4f0c92d
RISCi_ATOM <bob@bobcall.me>
2021-12-29 15:15:49 -0500

    base: Update python requirements

--- include/prereq-build.mk
+++ include/prereq-build.mk
@@ -145,11 +145,13 @@ $(eval $(call SetupHostCommand,perl,Please install Perl 5.x, \
 $(eval $(call CleanupPython2))
 
 $(eval $(call SetupHostCommand,python,Please install Python >= 3.5, \
+	python3.10 -V 2>&1 | grep 'Python 3', \
+	python3.9 -V 2>&1 | grep 'Python 3', \
 	python3.8 -V 2>&1 | grep 'Python 3', \
 	python3.7 -V 2>&1 | grep 'Python 3', \
 	python3.6 -V 2>&1 | grep 'Python 3', \
 	python3.5 -V 2>&1 | grep 'Python 3', \
-	python3 -V 2>&1 | grep -E 'Python 3\.[5-9]\.?'))
+	python3 -V 2>&1 | grep -E 'Python 3\.([5-9]|10)\.?'))
 
 $(eval $(call TestHostCommand,python3-jsmin, \
         Please install the jsmin Python module, \
