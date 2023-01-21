38be665d377b62b6c778a9d5c6d57f6cf58edeff
RISCi_ATOM <bob@bobcall.me>
2023-01-21 16:11:08 -0500

    kernel: Bump to 4.14.303

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,10 +6,10 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-LINUX_VERSION-4.14 = .302
+LINUX_VERSION-4.14 = .303
 LIBRE_REV = 1
 
-LINUX_KERNEL_HASH-4.14.302 = f2cb4c9172e5cadc7522ab6c3e373385c81ff12086364450162ea0c24c354fd8
+LINUX_KERNEL_HASH-4.14.303 = 4e39c980ff217cd4f9d7423303d46715a8ce36fcdd82b05ff6991b0e06fec5b5
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
