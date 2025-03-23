653b5312d8c00d8f8fdf44a20656db65458ac00b
RISCi_ATOM <bob@bobcall.me>
2025-03-23 13:10:48 -0400

    tools/ninja: Replace pipes module with shlex
    The pipes module was deprecated in python3.13. This commit replaces
    the pipes module with shlex in ninja and fixes build issues on distros
    that include Python3.13 or later.

--- include/prereq-build.mk
+++ include/prereq-build.mk
@@ -188,6 +188,8 @@ $(eval $(call SetupHostCommand,perl,Please install Perl 5.x, \
 	perl --version | grep "perl.*v5"))
 
 $(eval $(call SetupHostCommand,python,Please install Python >= 3.6, \
+	python3.13 -V 2>&1 | grep 'Python 3', \
+	python3.12 -V 2>&1 | grep 'Python 3', \
 	python3.11 -V 2>&1 | grep 'Python 3', \
 	python3.10 -V 2>&1 | grep 'Python 3', \
 	python3.9 -V 2>&1 | grep 'Python 3', \
